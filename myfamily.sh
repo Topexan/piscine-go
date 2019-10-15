f3956ff311c15ce91cdba711b7cbadbd46500c76
toktarovtorehan <t.torehan@gmail.com>
2019-10-15 20:43:10 +0600

    8 commit

--- myfamily.sh
+++ myfamily.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -s https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json | jq --argjson HID "$HERO_ID" '.[] | select (.id == $HID) | .connection.relatives'
+curl -s https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json | jq --argjson HID "$HERO_ID" '.[] | select (.id == $HID) | .connections.relatives'
