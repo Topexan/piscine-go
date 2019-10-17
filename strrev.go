96a3804b5bc9def95428a82940bb42fcd94587b0
toktarovtorehan <t.torehan@gmail.com>
2019-10-17 20:53:02 +0600

    strrev

---
+++ strrev.go
@@ -0,0 +1,17 @@
+package piscine
+
+func StrRev(s string) string {
+	runes := []rune(s)
+	var z rune
+	var count int = 0
+	for i := range runes {
+		count++
+		i = i
+	}
+	for l := 0; l <= count/2; l++ {
+		z = runes[l]
+		runes[l] = runes[count-l-1]
+		runes[count-l-1] = z
+	}
+	return string(runes)
+}
