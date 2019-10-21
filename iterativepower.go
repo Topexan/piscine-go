30c0414609d29772cfccd6ec852f5104dc9779d7
Topexan <54759948+Topexan@users.noreply.github.com>
2019-10-21 16:50:26 +0600

    iterativepower.go

---
+++ iterativepower.go
@@ -0,0 +1,15 @@
+package piscine
+
+func IterativePower(nb int, power int) int {
+	res := 1
+	if nb == 0 {
+		return 1
+	} else if nb < 0 || power < 0 {
+		return 0
+	} else {
+		for i := 0; i < power; i++ {
+			res = res * nb
+		}
+		return res
+	}
+}
