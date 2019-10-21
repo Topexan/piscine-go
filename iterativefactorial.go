907ca95c2e8c50735ba38f69846071afeeb35118
Topexan <54759948+Topexan@users.noreply.github.com>
2019-10-21 16:10:05 +0600

    iterativefactorial

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -2,7 +2,9 @@ package piscine
 
 func IterativeFactorial(nb int) int {
 	res := 1
-	if nb <= 0 || nb > 12 {
+	if nb == 0 {
+		return 1
+	} else if nb < 0 || nb > 12 {
 		return 0
 	} else {
 		for i := 1; i <= nb; i++ {
