ed9ac83c834ff6c5f62c19ba9a8ff81a0f8a5ba7
toktarovtorehan <t.torehan@gmail.com>
2019-10-21 20:48:15 +0600

    isprime

--- isprime.go
+++ isprime.go
@@ -3,6 +3,11 @@ package piscine
 func IsPrime(nb int) bool {
 	count := 0
 	t := 0
+	if nb == 0 || nb == 1 {
+		return false
+	} else if nb < 0 {
+		nb = nb * -1
+	}
 	for i := nb - 1; i > 1; i-- {
 		t = nb % i
 		if t == 0 {
