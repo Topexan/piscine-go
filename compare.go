f0371ae024b51c762737f1193754616ed9c49821
toktarovtorehan <t.torehan@gmail.com>
2019-10-22 23:23:48 +0600

    compare

---
+++ compare.go
@@ -0,0 +1,53 @@
+package piscine
+
+func Compare(a, b string) int {
+	runes1 := []rune(a)
+	runes2 := []rune(b)
+	var l1 int = 0
+	var l2 int = 0
+	for i := range runes1 {
+		l1++
+		i = i
+	}
+	for j := range runes2 {
+		l2++
+		j = j
+	}
+	num := l1
+	if l1 == 0 && l2 == 0 {
+		return 0
+	} else if l1 == 0 {
+		return -1
+	} else if l2 == 0 {
+		return 1
+	} else {
+		if l1 > l2 {
+			num = l2
+		}
+		for k := 0; k < num; k++ {
+			if runes1var r int = 0
+	for i := range runes {
+		r++
+		i = i
+	}[k] > runes2[k] {
+				returvar r int = 0
+	for i := range runes {
+		r++
+		i = i
+	}n 1
+			} else ifvar r int = 0
+	for i := range runes {
+		r++
+		i = i
+	} runes1[k] < runes2[k] {
+				return -1
+			}
+		}
+		if l1 == l2 {
+			return 0
+		} else if l1 > l2 {
+			return 1
+		}
+		return -1
+	}
+}
