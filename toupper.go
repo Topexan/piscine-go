326b7cc911128624535d33f0e522a7000ccef378
toktarovtorehan <t.torehan@gmail.com>
2019-10-22 23:47:15 +0600

    toupper

---
+++ toupper.go
@@ -0,0 +1,13 @@
+package piscine
+
+func ToUpper(s string) string {
+	runes := []rune(s)
+	for i := range runes {
+		if runes[i] > 96 && runes[i] < 123 {
+			runes[i] = runes[i] - 32
+		}
+		i = i
+	}
+	str := string(runes)
+	return str
+}
