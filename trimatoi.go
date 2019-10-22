0c0c8e3e8d26bad464c0ac39f27e5026da0721ab
toktarovtorehan <t.torehan@gmail.com>
2019-10-22 21:11:39 +0600

    trimatoi

--- trimatoi.go
+++ trimatoi.go
@@ -5,43 +5,49 @@ func TrimAtoi(s string) int {
 		return 0
 	}
 	var result int = 0
-	var isneg int = 0
+	isnum := 0
+	isneg := 0
 	runes := []rune(s)
-
-	for index, i := range runes {
-		if runes[index] == '-' && runes[index+1] > 47 && runes[index+1] < 58 {
-			isneg++
-			i = i
-		}
-	}
-
 	for _, j := range runes {
-		if j > 47 && j < 58 {
+		if j > 47 && j < 58 || j == '-' {
 			result *= 10
 			if j == '0' {
 				result += 0
 			} else if j == '1' {
 				result += 1
+				isnum++
 			} else if j == '2' {
 				result += 2
+				isnum++
 			} else if j == '3' {
 				result += 3
+				isnum++
 			} else if j == '4' {
 				result += 4
+				isnum++
 			} else if j == '5' {
 				result += 5
+				isnum++
 			} else if j == '6' {
 				result += 6
+				isnum++
 			} else if j == '7' {
 				result += 7
+				isnum++
 			} else if j == '8' {
 				result += 8
+				isnum++
 			} else if j == '9' {
 				result += 9
+				isnum++
+			} else if j == '-' && isnum == 0 {
+				isneg++
+			} else if j == '-' {
+				result /= 10
 			}
 		}
 	}
-	if isneg >= 1 {
+	if isneg > 0 {
 		result *= -1
 	}
 	return result
