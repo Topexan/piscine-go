0dd51c1aca11970e9c3d2da22c7fe7db4f9e0e82
toktarovtorehan <t.torehan@gmail.com>
2019-10-23 19:08:26 +0600

    printprogramname

---
+++ printprogramname/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/01-edu/z01"
+	"os"
+)
+
+func main() {
+	arguments := os.Args
+	str := arguments[0]
+	name := []rune(str)
+	for i := range name {
+		z01.PrintRune(name[i])
+	}
+}
