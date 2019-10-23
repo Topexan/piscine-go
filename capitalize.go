afacf93ff2d8eeb4d3f445bad3f4b6e1c85030a6
Topexan <54759948+Topexan@users.noreply.github.com>
2019-10-23 15:16:45 +0600

    capitalize

--- capitalize.go
+++ capitalize.go
@@ -2,19 +2,18 @@ package piscine
 
 func Capitalize(s string) string {
 	runes := []rune(s)
-	var r int = 0
-	for j := range runes {
-		r++
-		j = j
-	}
-	for i := 1; i < r; i++ {
-		if runes[i-1] == ' ' || runes[i-1] == '+' {
-			if runes[i] > 96 && runes[i] < 123 {
-				runes[i] = runes[i] - 32
-			}
+	isFirstLetter := true
+	for i := range runes {
+		if runes[i] >= 'a' && runes[i] <= 'z' && isFirstLetter {
+			runes[i] -= 32
+			isFirstLetter = false
+		} else if (IsRuneDigit(runes[i]) || (runes[i] >= 'A' && runes[i] <= 'Z')) && isFirstLetter {
+			isFirstLetter = false
+		} else if !isFirstLetter && runes[i] >= 'A' && runes[i] <= 'Z' {
+			runes[i] += 32
+		} else if !isFirstLetter && !IsRuneDigit(runes[i]) && !IsRuneAlpha(runes[i]) {
+			isFirstLetter = true
 		}
-		i = i
 	}
-	str := string(runes)
-	return str
+	return string(runes)
 }
