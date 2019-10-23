9497e2293f00455e4625894a830a0ff8044dc10d
toktarovtorehan <t.torehan@gmail.com>
2019-10-23 19:24:37 +0600

    revparams

---
+++ revparams/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/01-edu/z01"
+	"os"
+)
+
+func main() {
+	arguments := os.Args
+	l := 0
+	for i := range arguments {
+		l++
+		i = i
+	}
+	for j := l-1; j > 0; j-- {
+		runes := []rune(arguments[j])
+		for k := range runes {
+			z01.PrintRune(runes[k])
+		}
+		z01.PrintRune(10)
+	}
+}
