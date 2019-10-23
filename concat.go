d14f8ce638930f0b3d7af19e799d41ba560f142f
Topexan <54759948+Topexan@users.noreply.github.com>
2019-10-23 17:07:42 +0600

    concat

--- concat.go
+++ concat.go
@@ -1,15 +1,5 @@
 package piscine
 
 func Concat(str string, str2 string) string {
-	runes1 := []rune(str)
-	runes2 := []rune(str2)
-	var sen []rune
-	for i := range runes1 {
-		sen = append(sen, runes1[i])
-	}
-	for j := range runes2 {
-		sen = append(sen, runes2[j])
-	}
-
-	return string(sen)
+	return str + str2
 }
