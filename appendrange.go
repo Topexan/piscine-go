23da0988e9e4cc4b2bf35220055676b8eb39e6d9
Topexan <54759948+Topexan@users.noreply.github.com>
2019-10-24 10:10:59 +0600

    appendrange

--- appendrange.go
+++ appendrange.go
@@ -3,7 +3,7 @@ package piscine
 func AppendRange(min, max int) []int {
 	var ans []int
 	if min < max {
-		for i := min; i <= max; i++ {
+		for i := min; i < max; i++ {
 			ans = append(ans, i)
 		}
 	}
